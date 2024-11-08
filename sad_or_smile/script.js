9291937225e484aec57d9967c9ae810f2ccbe559
vitaliysummer2022 <107811758+vitaliysummer2022@users.noreply.github.com>
2024-11-08 09:36:30 +0200

    Download sad or smile

---
+++ sad_or_smile/script.js
@@ -0,0 +1,9 @@
+document.querySelector('img').addEventListener('click', () => {
+	document.querySelector('img').src = 'img/happy.png';
+	document.querySelector('span').textContent = 'I am happy';
+});
+
+
+
+// 'I am happy'
+// 'img/happy.png'
